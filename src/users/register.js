98dd6f97ce9d1cd027e1bba55db7b6bd64d326a0
runhanshi <ShiRunhan0329@outlook.com>
2022-12-07 11:27:05 -0800

    remove useless code in register

--- src/users/register.js
+++ src/users/register.js
@@ -27,23 +27,6 @@ const Register = () => {
 
     return(
         <>
-            {/*<h1>Register</h1>*/}
-            {/*<input*/}
-            {/*    onChange={(e) => setUsername(e.target.value)}*/}
-            {/*    className="form-control"*/}
-            {/*    placeholder="username"*/}
-            {/*    value={username}/>*/}
-            {/*<input*/}
-            {/*    onChange={(e) => setPassword(e.target.value)}*/}
-            {/*    className="form-control"*/}
-            {/*    placeholder="password"*/}
-            {/*    type="password"*/}
-            {/*    value={password}/>*/}
-            {/*<button*/}
-            {/*    className="btn btn-primary w-100"*/}
-            {/*    onClick={handleRegisterBtn}>*/}
-            {/*    Register*/}
-            {/*</button>*/}
             <div className="form_wrapper">
                 <h3 className="sign">Sign Up</h3>
                 <div className="mb-3">
