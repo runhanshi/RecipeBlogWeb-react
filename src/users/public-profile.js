4b037d64e8a9e740a45caf7bf5171245314fb89a
runhanshi <ShiRunhan0329@outlook.com>
2022-12-08 19:40:13 -0800

    public profile profile/uid

--- src/users/public-profile.js
+++ src/users/public-profile.js
@@ -1,14 +1,32 @@
 import {useParams} from "react-router";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {findUserByIdThunk} from "./users-thunk";
 import {Link} from "react-router-dom";
 import {findFollowersThunk, findFollowingThunk, followUserThunk} from "../follows/follows-thunks";
+import {findRecipeBySearchKeyThunk} from "../ext-recipe/ext-recipe-thunks";
+import {findUserById, getMyRecipes} from "./users-service";
 
 const PublicProfile = () => {
-
+    const {uid} = useParams()
+    const dispatch = useDispatch()
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        console.log(uid)
+        console.log(typeof uid)
+        findUserById(uid).then((res) => {
+            console.log("res = ", res);
+            setUser(res);
+        });
+    }, [])
     return(
-        <></>
+        <>
+            <p>Username: {user.username}</p>
+            <p>Firstname: {user.firstname}</p>
+            <p>Lastname: {user.lastname}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+        </>
     )
 }
 
