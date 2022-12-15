39afb6a51a69c120765df63f7bffc6e0d4710309
Your Name <you@example.com>
2022-12-15 02:15:07 -0500

    css

--- src/int-recipe/recipe-create-success.js
+++ src/int-recipe/recipe-create-success.js
@@ -14,8 +14,8 @@ const RecipeCreateSuccess = () => {
 
     return (
         <>
-            <h2 className="mb-2">Thank you {currentUser.username} !</h2>
-            <h4 className="font1 font">You just created a recipe for {recipeName}</h4>
+            <h2 className="mb-2 mt-4">Thank you {currentUser.username} !</h2>
+            <h4 className="font1 mt-4 mb-4">You just created a recipe for {recipeName}</h4>
             <Link to={`/recipes/${intRecipeID}`} className="">
                 View Recipe
             </Link>
